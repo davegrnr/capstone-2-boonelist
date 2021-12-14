d030af7fe1f619ffea14b96cb25b8406cb85c8b5
davegrnr <davegrnr@gmail.com>
2021-12-14 16:51:39 -0500

    Add static.json

--- boonelist-server/app.js
+++ boonelist-server/app.js
@@ -26,6 +26,14 @@ app.use("/users", usersRoutes);
 app.use("/services", servicesRoutes);
 app.use("/sales", salesRoutes);
 
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static('build'));
+//     app.get('*', (req, res) => {
+//         req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+//     })
+// }
+
+
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
@@ -34,13 +42,12 @@ app.use(function (req, res, next) {
 
     /** Generic error handler; anything unhandled goes here. */
 app.use(function (err, req, res, next) {
-if (process.env.NODE_ENV !== "test") console.error(err.stack);
-const status = err.status || 500;
-const message = err.message;
-
-return res.status(status).json({
-    error: { message, status },
-});
+        if (process.env.NODE_ENV !== "test") console.error(err.stack);
+            const status = err.status || 500;
+            const message = err.message;
+            return res.status(status).json({
+                error: { message, status },
+    });
 });
 
 module.exports = app;
